2d40d9b2494c055802d934c65b6a6468d289bf76
Rishav Bhattacharjee <rishavbhattacharjee777@gmail.com>
2023-07-02 18:55:47 +0530

    Database Connected

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const serverConfig = require('./configs/server.config');
+const mongoose = require('mongoose');
+const dbConfig = require('./configs/db.config');
 const app = express()
 
+/** Connect to MONGODB and create ADMIN user */
+
+mongoose.connect(dbConfig.DB_URL);
+const db = mongoose.connection;
+
+db.on("error", ()=>{
+    console.log(`ERROR while connecting to DB`);
+});
+
+db.once("open", ()=>{
+    console.log(`DB is connected`);
+})
 
 app.listen(serverConfig.PORT, ()=>{
-    console.log(`server started on port no ${serverConfig.PORT}`)
+    console.log(`server started on port no ${serverConfig.PORT}`);
 })
